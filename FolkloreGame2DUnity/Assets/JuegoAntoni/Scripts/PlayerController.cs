a36299c6256e8b6a63e1d93fdbf83b141d574bbe
antonimf17 <antonimoll.20novembre@gmail.com>
2025-01-30 17:17:13 +0100

    a
    a

--- FolkloreGame2DUnity/Assets/JuegoAntoni/Scripts/PlayerController.cs
+++ FolkloreGame2DUnity/Assets/JuegoAntoni/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      
+
         anim = GetComponent<Animator>();
         PlayerRB = GetComponent<Rigidbody2D>();
 
